805cebe4145f1b31ffdbd0bc2bb74ead81d23fb0
GiovanaMv <saddnez@gmail.com>
2025-03-25 11:51:34 -0300

    componentização da aplicação

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,20 @@
+<script setup>
+    defineProps(['tarefasPendentes']);
+</script>
+
+<template>
+    <header class="p-3 pt-4 mb-4 mt-4  rounded-3">
+        <h1>Minha Lista de SuperMercado 🛒</h1>
+        <p>
+            Você possui {{ tarefasPendentes }} itens a buscar🍊🍉🍇
+        </p>
+    </header>
+</template>
+
+<style>
+    header{
+        background-color: rgb(255, 252, 93);
+        text-align: center;
+    }
+    
+</style>
